4972b2d9ff8c7aa13d5502b89301c494b3a4483a
Keijiro Takahashi <keijiro@MSX.local>
2013-10-10 07:18:28 +0900

    Renamed.

--- ObliqueProjection.cs
+++ ObliqueProjection.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 [ExecuteInEditMode, RequireComponent(typeof(Camera))]
-public class CabinetProjection : MonoBehaviour
+public class ObliqueProjection : MonoBehaviour
 {
     public float angle = 45.0f;
-    public float zScale = 0.2f;
-    public float zOffset = 5.0f;
+    public float zScale = 0.5f;
+    public float zOffset = 0.0f;
 
     public void Apply ()
     {
@@ -14,7 +14,7 @@ public class CabinetProjection : MonoBehaviour
         var orthoHeight = camera.orthographicSize;
         var orthoWidth = camera.aspect * orthoHeight;
         var m = Matrix4x4.Ortho (-orthoWidth, orthoWidth, -orthoHeight, orthoHeight, camera.nearClipPlane, camera.farClipPlane);
-        var s = zScale * 0.5f / orthoHeight;
+        var s = zScale / orthoHeight;
         m [0, 2] = +s * Mathf.Sin (Mathf.Deg2Rad * -angle);
         m [1, 2] = -s * Mathf.Cos (Mathf.Deg2Rad * -angle);
         m [0, 3] = -zOffset * m [0, 2];
--- ObliqueProjectionEditor.cs
+++ ObliqueProjectionEditor.cs
@@ -2,15 +2,15 @@
 using UnityEditor;
 using System.Collections;
 
-[CustomEditor(typeof(CabinetProjection))]
-public class CabinetProjectionEditor : Editor
+[CustomEditor(typeof(ObliqueProjection))]
+public class ObliqueProjectionEditor : Editor
 {
     public override void OnInspectorGUI ()
     {
-        var t = target as CabinetProjection;
+        var t = target as ObliqueProjection;
 
         GUI.changed = false;
-        t.angle = EditorGUILayout.Slider ("Angle", t.angle, -360.0f, 360.0f);
+        t.angle = EditorGUILayout.Slider ("Angle", t.angle, -180.0f, 180.0f);
         t.zScale = EditorGUILayout.Slider ("Z Scale", t.zScale, 0.0f, 1.0f);
         t.zOffset = EditorGUILayout.FloatField ("Z Offset", t.zOffset);
 
